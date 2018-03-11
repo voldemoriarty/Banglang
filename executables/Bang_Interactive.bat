1aa174ca2162e2cff329335d5bfd877b36215815
saad <msaad1200@gmail.com>
2018-03-11 14:38:26 +0500

    clean up

--- executables/Bang_Interactive.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-echo Choose from 'Examples' folder or make a new text file.
-echo [Demo] Enter Source: "Examples\Calculate_e.txt"
-echo.
-set /p source=Enter Source:
-echo %source%
-bangc %source% blabla
-echo.
-pause
